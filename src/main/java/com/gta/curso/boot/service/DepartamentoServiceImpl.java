59aab4e52cefdd63490778bf949d9df7ed194b4b
guilherme.adaniya <guilherme.adaniya@resource.com.br>
2020-02-06 17:20:39 -0300

    Dao and Services

---
+++ src/main/java/com/gta/curso/boot/service/DepartamentoServiceImpl.java
@@ -0,0 +1,43 @@
+package com.gta.curso.boot.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gta.curso.boot.dao.DepartamentoDao;
+import com.gta.curso.boot.domain.Departamento;
+
+@Service
+@Transactional(readOnly = false)
+public class DepartamentoServiceImpl implements DepartamentoService{
+
+	@Autowired
+	private DepartamentoDao dao;
+
+	@Override
+	public void salvar(Departamento departamento) {
+		dao.save(departamento);
+	}
+
+	@Override
+	public void editar(Departamento departamento) {
+		dao.update(departamento);
+	}
+
+	@Override
+	public void excluir(Long id) {
+		dao.delete(id);
+	}
+
+	@Override @Transactional(readOnly = true)
+	public Departamento buscarPorId(Long id) {
+		return dao.findById(id);
+	}
+
+	@Override @Transactional(readOnly = true)
+	public List<Departamento> buscarTodos() {
+		return dao.findAll();
+	}
+}
